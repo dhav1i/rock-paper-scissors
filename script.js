45e30ae1bca4243b5c529f8de0096232ff18d3d5
dhavi <222001371+dhav1i@users.noreply.github.com>
2025-08-10 05:00:20 +0300

    Add user interface to the game Players can now play the rock-paper-scissors game with buttons instead of text prompt.

--- script.js
+++ script.js
@@ -1,52 +1,81 @@
+const humanScoreText = document.getElementById('humanScoreText')
+const computerScoreText = document.getElementById('computerScoreText')
+const humanChoiceText = document.getElementById('humanChoiceText')
+const computerChoiceText = document.getElementById('computerChoiceText')
+const matchResultText = document.getElementById('matchResultText')
+const roundResultText = document.getElementById('roundResultText')
+const humanChoiceBtns = document.querySelectorAll('.humanChoiceBtn')
+
 let humanScore = 0;
 let computerScore = 0;
 
+
 function getCopmuterChoice() {
-let randomChoice = Math.floor(Math.random() * 3)
+ let randomChoice = Math.floor(Math.random() * 3);
     if(randomChoice == 0) {
-        return "rock"
+        return "rock";
     } else if(randomChoice == 1) {
-        return "paper"
+        return "paper";
     } else {
-        return "scissors"
-    }
+        return "scissors";
+    };
 }
 
-function getHumanChoice() {
-let userChoice = prompt("Choose 'Rock', 'Paper', or 'Scissors'.").toLowerCase()
-        return userChoice
-}
+let computerSelection = getCopmuterChoice();
 
 function playRound(humanChoice, computerChoice) {
+ let roundResult;
     if(computerChoice === humanChoice) {
-        console.log(`It's a tie! ${humanChoice} ties with ${computerChoice}`)
-    } else if (computerChoice == 'rock' && humanChoice == 'scissors' || computerChoice == 'scissors' && humanChoice == 'paper' || computerChoice == 'paper' && humanChoice == 'rock') {
-        computerScore++
-        console.log(`You Lose! ${computerChoice} beats ${humanChoice}`)
-    } else if (humanChoice == 'rock' && computerChoice == 'scissors' || humanChoice == 'scissors' && computerChoice == 'paper' || humanChoice == 'paper' && computerChoice == 'rock'){
-        humanScore++
-        console.log(`Congrats you won! ${humanChoice} beats ${computerChoice}`)
+        roundResult = `It's a tie! ${humanChoice[0].toUpperCase() + humanChoice.substring(1)} ties with ${computerChoice[0].toUpperCase() + computerChoice.substring(1)}` 
+        roundResultText.textContent = roundResult
+    } else if (computerChoice == 'rock' && humanChoice == 'scissors' || 
+            computerChoice == 'scissors' && humanChoice == 'paper' || 
+            computerChoice == 'paper' && humanChoice == 'rock') {
+        computerScore++;
+        roundResult = `You lose! ${computerChoice[0].toUpperCase() + computerChoice.substring(1)} beats ${humanChoice[0].toUpperCase() + humanChoice.substring(1)}`
+        roundResultText.textContent = roundResult;
+    } else if (humanChoice == 'rock' && computerChoice == 'scissors' ||
+            humanChoice == 'scissors' && computerChoice == 'paper' ||
+            humanChoice == 'paper' && computerChoice == 'rock'){
+        humanScore++;
+        roundResult = `You won! ${humanChoice[0].toUpperCase() + humanChoice.substring(1)} beats ${computerChoice[0].toUpperCase() + computerChoice.substring(1)}`;
+        roundResultText.textContent = roundResult;
+    };
+   
+    let matchResult;
+    if(computerScore == 5 && computerScore > humanScore) {
+    matchResult = 'Computer won! good luck next time.';
+    matchResultText.textContent = matchResult;
+    } else if(humanScore == 5 && computerScore < humanScore){
+        matchResult = 'Congrats! you domainated the computer.';
+        matchResultText.textContent = matchResult;
     }
-     else {
-        console.log("Please enter a valid option 'Rock', 'Paper', or 'Scissors' ")
-     }
-    console.log(`Your score is: ${humanScore}, Computer score is: ${computerScore}`)
+
+    computerChoiceText.textContent = `Computer: ${computerChoice[0].toUpperCase() + computerChoice.substring(1)}`;
+    humanChoiceText.textContent = `Human: ${humanChoice[0].toUpperCase() + humanChoice.substring(1)}`;
+    humanScoreText.textContent = `Human score: ${humanScore}`
+    computerScoreText.textContent = `Computer score: ${computerScore}`
+    
 }
 
-function playGame() {
-    for(i = 0; i < 5; i++) {
-      playRound(getHumanChoice(), getCopmuterChoice())
-    }
-    if(computerScore > humanScore) {
-        console.log('Computer won! good luck next time')
-    } else if(computerScore < humanScore){
-        console.log('Congrats! you domainated the computer!')
+humanChoiceBtns.forEach((btn) => {
+    let playerSelecion;
+    btn.addEventListener('click', () => {
+    if(btn.id == "rock") {
+    playerSelecion = "rock";
+    } else if(btn.id == "paper") {
+    playerSelecion = "paper" ;
     } else {
-        console.log('No winners!')
+    playerSelecion = "scissors";
     }
-}
-
-playGame()
+    if(computerScore == 5 || humanScore == 5) {
+        return;
+    } else {
+        playRound(playerSelecion, computerSelection);
+    }
+     return;
+    })
+})
 
 
 
